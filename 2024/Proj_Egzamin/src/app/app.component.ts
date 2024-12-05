e95e2a92ac8b6910aadaf1bb480e70c8bdba87cb
Susel <94788268+UrSusel@users.noreply.github.com>
2024-12-05 17:46:56 +0100

    fixed some functions

--- 2024/Proj_Egzamin/src/app/app.component.ts
+++ 2024/Proj_Egzamin/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Proj_Egzamin';
+  title = 'Zadanie';
+  imie:string="";
+  Wybrany1:boolean[]=[];
+  Wybrany2:boolean[]=[];
+  Wybrany3:string="";
+  ryby:any=["Szczupak","Łosoś","Pstrąg","Mały łosoś"];
+  psy:any=[{id:1,value:"Kundel"},{id:2,value:"Szczeniak"},{id:3,value:"Buldog"},{id:4,value:"Pitbull"}];
+  warzywa:any=["Pomidor","Szczypior","Cebula","Bananowy Ogórek"];
+  wyswietlanie:boolean=false;
+  wyswietl(){
+    this.wyswietlanie=true;
+  }
 }
